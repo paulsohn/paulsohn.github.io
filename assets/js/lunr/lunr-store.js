702dc0530e91a3b1389b6e3b14bcb30e17d006fb
Taeseung Sohn <paulsohn@outlook.kr>
2022-11-16 07:56:47 +0000

    Site updated at 2022-11-16 07:56:47 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Setting up Jekyll Blog",
+        "excerpt":"Setting up Jekyll Blog Installation and Initialization I created a paulsohn.github.io repo which remote is on github. Necessary installations are: $ sudo apt install ruby $ sudo apt install gem $ gem install jekyll bundler It takes some time. After that, initialize jekyll blog. $ jekyll new --skip-bundle . Once...","categories": ["dev"],
+        "tags": [],
+        "url": "/dev/2022/08/23/setting-up-jekyll-blog.html",
+        "teaser": null
+      },{
+        "title": "CSAPP - Lab 1. (32-bit) Data Lab",
+        "excerpt":"Data Lab A lab assignment to implement primitive operations with (mostly) bitwise operations only. int type is assumed to be 32-bit. Integer Coding Common Techniques an all-set bit mask: ~0 a bit mask from x, full of its sign bit: x &gt;&gt; 31 isTmax Check whether the argument x is...","categories": ["arch"],
+        "tags": ["arch","CSAPP"],
+        "url": "/arch/2022/10/05/CSAPP-lab1-datalab.html",
+        "teaser": null
+      },{
+        "title": "CSAPP - Finished Reading",
+        "excerpt":"I Finished ‘Reading’ CSAPP!   Randal E. Bryant &amp; David R. O’Hallaron, Computer Systems: A Programmer’s Perspective, 3rd Global Edition (CS:APP3e)   2022-07-28 ~ 2022-10-08, not including its practices or labs.   Now it’s time to go and start coding!  ","categories": ["arch"],
+        "tags": ["arch","CSAPP"],
+        "url": "/arch/2022/10/18/CSAPP-finished-reading.html",
+        "teaser": null
+      },{
+        "title": "CSAPP - Lab 4. Architecture Lab",
+        "excerpt":"Arch Lab A lab assignment using Y86-64 ISA, an educational-purpose simplified version of modern x86-64 architecture. The lab consists of three parts: A. Write a Y86-64 assembly program to perform sum over linked list(both iterative and recursive), and copying memory. B. Modify sequential SEQ processor to support iaddq instruction. This...","categories": ["arch"],
+        "tags": ["arch","CSAPP"],
+        "url": "/arch/2022/11/12/CSAPP-lab4-archlab.html",
+        "teaser": null
+      },{
+        "title": "Jekyll: Build Locally and Push into Github",
+        "excerpt":"Motivation I wanted to use jekyll-archives in my blog to autogenerate index pages for each category. This worked on my local machine, but when I published the changes, I soon realized that my expected category-wise index pages are not shown, and instead I got 404. So I did some research,...","categories": ["dev"],
+        "tags": [],
+        "url": "/dev/2022/11/15/local-build-jekyll.html",
+        "teaser": null
+      }]
